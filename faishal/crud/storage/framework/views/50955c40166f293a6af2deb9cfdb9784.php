960786a7e31f76f0e1ef60111042eff19654d9b4
mhmmdalif <155944019+mhmmdalif@users.noreply.github.com>
2024-01-26 15:49:31 +0700

    fys

--- faishal/crud/storage/framework/views/50955c40166f293a6af2deb9cfdb9784.php
+++
@@ -1,7 +0,0 @@
-<script src="/assets/js/bootstrap.js"></script>
-    <script src="/assets/js/app.js"></script>
-
-    <!-- Need: Apexcharts -->
-    <script src="/assets/extensions/apexcharts/apexcharts.min.js"></script>
-    <script src="/assets/js/pages/dashboard.js"></script>
-<?php /**PATH D:\Git\smkturen\faishal\crud\resources\views/layouts/script.blade.php ENDPATH**/ ?>
