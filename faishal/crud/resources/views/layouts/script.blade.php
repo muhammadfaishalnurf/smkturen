e1cfb4551922b7f7ddf89477a39972618c128047
muhammadfaishalnurf <155943817+muhammadfaishalnurf@users.noreply.github.com>
2024-01-26 15:30:48 +0700

    Delete faishal directory
    hapus

--- faishal/crud/resources/views/layouts/script.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="/assets/js/bootstrap.js"></script>
-    <script src="/assets/js/app.js"></script>
-
-    <!-- Need: Apexcharts -->
-    <script src="/assets/extensions/apexcharts/apexcharts.min.js"></script>
-    <script src="/assets/js/pages/dashboard.js"></script>
